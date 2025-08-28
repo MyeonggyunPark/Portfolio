17961cb78e4511003006af09e3e86b91bbb72000
myeonggyun <audrbs92@gmail.com>
2025-08-28 15:54:55 +0200

    feat(urls): add course_detail URL route

--- KIMSDEUTSCH_WEB/web/urls.py
+++ KIMSDEUTSCH_WEB/web/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("course/", views.course_list, name="course_list"),
     path("create/", views.course_create, name="course_create"),
+    path("detail/<int:pk>/", views.course_detail, name="course_detail"),
 ]
