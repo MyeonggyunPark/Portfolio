c63e05a7292c20043f1d6484ea95bd7f5a8f7f81
myeonggyun <audrbs92@gmail.com>
2025-08-29 12:17:08 +0200

    feat(urls): add routes for course update and delete

--- KIMSDEUTSCH_WEB/web/urls.py
+++ KIMSDEUTSCH_WEB/web/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.main, name="main"),
     path("course/", views.course_list, name="course_list"),
-    path("create/", views.course_create, name="course_create"),
-    path("detail/<int:pk>/", views.course_detail, name="course_detail"),
+    path("course/create/", views.course_create, name="course_create"),
+    path("course/<int:pk>/", views.course_detail, name="course_detail"),
+    path("course/<int:pk>/update/", views.course_update, name="course_update"),
+    path("course/<int:pk>/delete/", views.course_delete, name="course_delete"),
 ]
